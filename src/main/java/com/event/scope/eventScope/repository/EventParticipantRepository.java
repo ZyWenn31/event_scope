a081119b8ea72ec507e1370e7ce9c8f9ef50fdbf
ZyWenn31 <Topchic525@gmail.com>
2026-01-31 13:45:27 +0300

    feat: replace pages view

--- src/main/java/com/event/scope/eventScope/repository/EventParticipantRepository.java
+++ src/main/java/com/event/scope/eventScope/repository/EventParticipantRepository.java
@@ -1,9 +1,19 @@
 package com.event.scope.eventScope.repository;
 
+import com.event.scope.eventScope.model.Event;
 import com.event.scope.eventScope.model.EventParticipant;
+import com.event.scope.eventScope.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EventParticipantRepository extends JpaRepository<EventParticipant, Long> {
+    boolean existsByUserAndEvent(User user, Event event);
+    Optional<EventParticipant> findByUserAndEvent(User user, Event event);
+
+    List<EventParticipant> findAllByUser(User user);
 }
